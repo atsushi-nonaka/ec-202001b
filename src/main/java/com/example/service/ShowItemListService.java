091f0725d93089eba0384aad595374b86125db77
tsuchiya <hiromu.tsuchiya@rakus-partners.co.jp>
2020-03-02 23:06:52 +0900

    @serviceを記述し忘れた為、再度コミット致しました。

--- src/main/java/com/example/service/ShowItemListService.java
+++ src/main/java/com/example/service/ShowItemListService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.domain.Item;
 import com.example.repository.ItemRepository;
@@ -13,6 +14,7 @@ import com.example.repository.ItemRepository;
  * @author tsuchiyahiromu
  *
  */
+@Service
 public class ShowItemListService {
 	@Autowired
 	private ItemRepository itemRepository;
