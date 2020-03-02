20a28f248e1dd9c500037da658cf1d10d3d12b22
tsuchiya <hiromu.tsuchiya@rakus-partners.co.jp>
2020-03-02 22:33:57 +0900

    (初級)3.商品一覧を表示する、検索する ItemRepositoryを作成しました。 全件検索及び条件検索（曖昧検索）をfindByLikeName()メソッドに実装しました。 SQLの実施等は行っておりませんので、正常に動作するかは未確認です。

---
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -0,0 +1,52 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Item;
+
+/**
+ * itemsテーブルを操作するレポジトリ.
+ * 
+ * @author tsuchiyahiromu
+ *
+ */
+@Repository
+public class ItemRepository {
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	/**
+	 * Itemオブジェクト生成するローマッパー.
+	 */
+	private static final RowMapper<Item> ITEM_ROW_MAPPER = (rs, i) -> {
+		Item item = new Item();
+		item.setId(rs.getInt("id"));
+		item.setName(rs.getString("name"));
+		item.setDescription(rs.getString("description"));
+		item.setPriceM(rs.getInt("price_m"));
+		item.setPriceL(rs.getInt("price_l"));
+		item.setImagePath(rs.getString("image_path"));
+		item.setDeleted(rs.getBoolean("deleted"));
+		return item;
+	};
+
+	/**
+	 * 商品名から商品情報を取得します.
+	 * 
+	 * @param name 商品名
+	 * @return 商品情報を返します
+	 */
+	public List<Item> findByLikeName(String name) {
+		String sql = "SELECT id,name,description,price_m,price_l,image_path,deleted FROM items where name = :name";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%" + name + "%");
+		List<Item> itemList = template.query(sql, param, ITEM_ROW_MAPPER);
+		return itemList;
+	}
+}
