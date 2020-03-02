2ecb2238172c6d424b0eb7f56c2feb9e71a9b6a9
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2020-03-02 17:19:42 +0900

    first commit

---
+++ src/main/java/com/example/domain/Order.java
@@ -0,0 +1,163 @@
+package com.example.domain;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * 注文情報を表すドメインクラス.
+ * 
+ * @author nonaka
+ *
+ */
+public class Order {
+	/** ID */
+	private Integer id;
+	/** ユーザーID */
+	private Integer userId;
+	/** 状態 */
+	private Integer status;
+	/** 合計金額 */
+	private Integer totalPrice;
+	/** 注文日 */
+	private Date orderDate;
+	/** 宛先氏名 */
+	private String destinationName;
+	/** 宛先Eメール */
+	private String destinationEmail;
+	/** 宛先郵便番号 */
+	private String destinationZipcode;
+	/** 宛先住所 */
+	private String destinationAddress;
+	/** 宛先TEL */
+	private String destinationTel;
+	/** 配達時間 */
+	private String deliveryTime;
+	/** 支払方法 */
+	private String paymentMethod;
+	/** ユーザー */
+	private User user;
+	/** 注文商品リスト */
+	private List<OrderItem> orderList;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Integer getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Integer totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public String getDestinationName() {
+		return destinationName;
+	}
+
+	public void setDestinationName(String destinationName) {
+		this.destinationName = destinationName;
+	}
+
+	public String getDestinationEmail() {
+		return destinationEmail;
+	}
+
+	public void setDestinationEmail(String destinationEmail) {
+		this.destinationEmail = destinationEmail;
+	}
+
+	public String getDestinationZipcode() {
+		return destinationZipcode;
+	}
+
+	public void setDestinationZipcode(String destinationZipcode) {
+		this.destinationZipcode = destinationZipcode;
+	}
+
+	public String getDestinationAddress() {
+		return destinationAddress;
+	}
+
+	public void setDestinationAddress(String destinationAddress) {
+		this.destinationAddress = destinationAddress;
+	}
+
+	public String getDestinationTel() {
+		return destinationTel;
+	}
+
+	public void setDestinationTel(String destinationTel) {
+		this.destinationTel = destinationTel;
+	}
+
+	public String getDeliveryTime() {
+		return deliveryTime;
+	}
+
+	public void setDeliveryTime(String deliveryTime) {
+		this.deliveryTime = deliveryTime;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<OrderItem> getOrderList() {
+		return orderList;
+	}
+
+	public void setOrderList(List<OrderItem> orderList) {
+		this.orderList = orderList;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", userId=" + userId + ", status=" + status + ", totalPrice=" + totalPrice
+				+ ", orderDate=" + orderDate + ", destinationName=" + destinationName + ", destinationEmail="
+				+ destinationEmail + ", destinationZipcode=" + destinationZipcode + ", destinationAddress="
+				+ destinationAddress + ", destinationTel=" + destinationTel + ", deliveryTime=" + deliveryTime
+				+ ", paymentMethod=" + paymentMethod + ", user=" + user + "]";
+	}
+
+}
