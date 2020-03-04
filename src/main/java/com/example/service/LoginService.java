1f983b24a30f79ecf1a2a66041ef0a0f6be194fe
yamasekishoya <shoya.4617@gmail.com>
2020-03-05 01:12:50 +0900

    微修正

--- src/main/java/com/example/service/LoginService.java
+++ src/main/java/com/example/service/LoginService.java
@@ -7,15 +7,28 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.domain.User;
 import com.example.repository.UserRepository;
 
+/**
+ * ログイン情報をリポジトリに渡す役割のサービスクラスです.
+ * 
+ * @author yamaseki
+ *
+ */
 @Service
 @Transactional
 public class LoginService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public User login(String email,String password) {
-		User user = userRepository.findByEmailAndPassword(email,password);
+
+	/**
+	 * ログインメソッドです.
+	 * 
+	 * @param email 
+	 * @param password
+	 * @return ユーザー情報を返します
+	 */
+	public User login(String email, String password) {
+		User user = userRepository.findByEmailAndPassword(email, password);
 		return user;
 	}
 
