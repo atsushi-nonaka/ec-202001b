a3b6f5236ef3a45813e2a0881b4d9cb421d9823f
yamasekishoya <shoya.4617@gmail.com>
2020-03-06 20:42:12 +0900

    パスの変更

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests() // 認可に関する設定
-			.antMatchers("/rakuraku-pizza/toLogin","/show_item_list","/insert","/insert/insertUser","/show_item_detail").permitAll() //「/」などのパスは全てのユーザに許可
+			.antMatchers("/rakuraku-pizza/toLogin","/","/insert","/insert/insertUser","/show_item_detail").permitAll() //「/」などのパスは全てのユーザに許可
 			//.antMatchers("/admin/**").hasRole("ADMIN") // /admin/から始まるパスはADMIN権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
 			//.antMatchers("/user/**").hasRole("USER") // /user/から始まるパスはUSER権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
 			.anyRequest().authenticated(); // それ以外のパスは認証が必要
@@ -63,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.loginPage("/rakuraku-pizza/toLogin") // ログイン画面に遷移させるパス(ログイン認証が必要なパスを指定してかつログインされていないとこのパスに遷移される)
 			.loginProcessingUrl("/login") // ログインボタンを押した際に遷移させるパス(ここに遷移させれば自動的にログインが行われる)
 			.failureUrl("/rakuraku-pizza/toLogin?error=true") //ログイン失敗に遷移させるパス
-			.defaultSuccessUrl("/show_item_list", true) // 第1引数:デフォルトでログイン成功時に遷移させるパス
+			.defaultSuccessUrl("/", true) // 第1引数:デフォルトでログイン成功時に遷移させるパス
 			                                        // 第2引数: true :認証後常に第1引数のパスに遷移 
 			                                        //         false:認証されてなくて一度ログイン画面に飛ばされてもログインしたら指定したURLに遷移
 			.usernameParameter("email") // 認証時に使用するユーザ名のリクエストパラメータ名(今回はメールアドレスを使用)
