f445207d6470ed339c73816338e96783037b1ed2
okadayuri <chemistory.12@gmail.com>
2020-03-10 13:53:31 +0900

    ページ、表の背景色を変更

--- src/main/java/com/example/service/DeleteItemService.java
+++ src/main/java/com/example/service/DeleteItemService.java
@@ -10,6 +10,11 @@ import com.example.repository.OrderItemRepository;
 import com.example.repository.OrderRepository;
 import com.example.repository.OrderToppingRepository;
 
+/**
+ * DB上のデータ削除の操作を担当するサービス.
+ * @author yamasaki
+ *
+ */
 @Service
 @Transactional
 public class DeleteItemService {
@@ -25,24 +30,45 @@ public class DeleteItemService {
 	@Autowired
 	private OrderToppingRepository orderToppingRepository;
 	
+	/**
+	 *注文商品IDの注文商品情報を取得します.
+	 * @param orderItemId 注文商品ID
+	 * @return 注文商品
+	 */
 	public OrderItem searchById(Integer orderItemId) {
 		OrderItem orderItem = orderItemRepository.findById(orderItemId);
 		return orderItem;
 	}
 	
+	/**
+	 * ユーザIDの注文情報を削除します.
+	 * @param userId ユーザID
+	 */
 	public void deleteOrderByUserId(Integer userId) {
 		orderRepository.deleteByUserId(userId);
 
 	}
 	
+	/**
+	 * 注文商品IDの注文商品情報を削除します.
+	 * @param id 注文商品ID
+	 */
 	public void deleteOrderItemById(Integer id) {
 		orderItemRepository.deleteById(id);
 	}
 	
+	/**
+	 * 注文トッピングIDの注文トッピング情報を削除します.
+	 * @param orderItemId 注文トッピングID
+	 */
 	public void deleteOrderToppingById(Integer orderItemId) {
 		orderToppingRepository.deleteByOrderId(orderItemId);;
 	}
 	
+	/**
+	 * 注文情報を更新します.
+	 * @param order 注文情報
+	 */
 	public void updateOrder(Order order) {
 		orderRepository.update(order);
 	}
